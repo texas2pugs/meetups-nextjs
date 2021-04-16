d4d9f71302ef401530c1829f4ccf038753d8a0a7
Christopher Inman <christophereinman@gmail.com>
2021-04-16 07:52:59 -0500

    Added MongoDB client

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,27 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const mongodb_user = 'testadmin';
+    const mongodb_pass = '4xl4Wzawo36sRxKn';
+    const mongodb_cluster = 'cluster0.j3l7i.mongodb.net';
+    const mongodb_database = 'next-meetups';
+
+    const connectionString = `mongodb+srv://${mongodb_user}:${mongodb_pass}@${mongodb_cluster}/${mongodb_database}?retryWrites=true&w=majority`;
+
+    const client = await MongoClient.connect(connectionString);
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted' });
   }
 }
 
