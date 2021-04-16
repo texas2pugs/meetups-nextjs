2e967a6e6a4e6d51135e10c857bfc0d8f4ef9486
Christopher Inman <christophereinman@gmail.com>
2021-04-16 08:19:47 -0500

    Getting all meetups from Mongo

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { MongoClient } from 'mongodb';
+
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -25,9 +27,30 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  const mongodb_user = 'testadmin';
+  const mongodb_pass = '4xl4Wzawo36sRxKn';
+  const mongodb_cluster = 'cluster0.j3l7i.mongodb.net';
+  const mongodb_database = 'next-meetups';
+
+  const connectionString = `mongodb+srv://${mongodb_user}:${mongodb_pass}@${mongodb_cluster}/${mongodb_database}?retryWrites=true&w=majority`;
+
+  const client = await MongoClient.connect(connectionString);
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 120,
   };
